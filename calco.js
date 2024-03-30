729e82ae6d56a678599c57d5ef9b1ceb4d3f5a17
junior2670 <koffikouameothiniel4@gmail.com>
2024-03-30 15:53:37 +0100

    initialisation du depot

---
+++ calco.js
@@ -0,0 +1,7 @@
+console.log(document)
+var larg=document.querySelector("larg-cara")
+var small= document.querySelector("small-cara")
+var cal1=document.querySelector("cal1")
+cal1.onclick=function(E){
+    larg.innerText="C"
+}
